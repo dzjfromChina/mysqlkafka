eec58b279201a43faf3ea26ff6656bc63213025f
duzj <dzj0574@163.com>
2019-07-07 11:46:20 +0800

    实时导入kafka

---
+++ src/main/java/com/mysqlkafka/mysqlkafka/MysqlkafkaApplication.java
@@ -0,0 +1,14 @@
+package com.mysqlkafka.mysqlkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(
+				MysqlkafkaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mysqlkafka/mysqlkafka/main/BinlogClientRunner.java
@@ -0,0 +1,157 @@
+package com.mysqlkafka.mysqlkafka.main;
+
+import com.alibaba.fastjson.JSON;
+import com.github.shyiko.mysql.binlog.BinaryLogClient;
+import com.github.shyiko.mysql.binlog.event.*;
+import com.mysqlkafka.mysqlkafka.kafka.KafkaProducerDemo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ * @author dzj
+ * @create 2019-07-06 17:40
+ */
+@Component
+public class BinlogClientRunner implements CommandLineRunner {
+
+    @Value("${binlog.host}")
+    private String host;
+
+    @Value("${binlog.port}")
+    private int port;
+
+    @Value("${binlog.user}")
+    private String user;
+
+    @Value("${binlog.password}")
+    private String password;
+
+    // binlog server_id
+    @Value("${server.id}")
+    private long serverId;
+
+    // kafka话题
+    @Value("${kafka.topic}")
+    private String topic;
+
+    // kafka分区
+    @Value("${kafka.partNum}")
+    private int partNum;
+
+    // Kafka备份数
+    @Value("${kafka.repeatNum}")
+    private short repeatNum;
+
+    // kafka地址
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaHost;
+
+    // 指定监听的数据表
+    @Value("${binlog.database.table}")
+    private String database_table;
+/*
+    @Autowired
+    KafkaSender kafkaSender;*/
+
+    @Async
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("开始---------------------------");
+        // 创建topic
+        //kafkaSender.createTopic(kafkaHost, topic, partNum, repeatNum);
+        // 获取监听数据表数组
+        List<String> databaseList = Arrays.asList(database_table.split(","));
+        System.out.println(databaseList );
+        HashMap<Long, String> tableMap = new HashMap<Long, String>();
+        // 创建binlog监听客户端
+        BinaryLogClient client = new BinaryLogClient(host, port, user, password);
+        client.setServerId(serverId);
+        client.registerEventListener((event -> {
+            // binlog事件
+            EventData data = event.getData();
+            if (data != null) {
+                //System.out.println(data);
+                if (data instanceof TableMapEventData) {
+                    TableMapEventData tableMapEventData = (TableMapEventData) data;
+                    String database = tableMapEventData.getDatabase();
+                    if(database.equals("test")){
+                        System.out.println("database:"+database);
+                    }
+                    tableMap.put(tableMapEventData.getTableId(), tableMapEventData.getDatabase() + "." + tableMapEventData.getTable());
+                }
+                // update数据
+                if (data instanceof UpdateRowsEventData) {
+                    UpdateRowsEventData updateRowsEventData = (UpdateRowsEventData) data;
+                    List<Map.Entry<Serializable[], Serializable[]>> rows = updateRowsEventData.getRows();
+                    String tableName = tableMap.get(updateRowsEventData.getTableId());
+                    if (tableName != null && databaseList.contains(tableName)) {
+                        for (Map.Entry<Serializable[], Serializable[]> row : updateRowsEventData.getRows()) {
+                            Map<String,Object> map = new HashMap<>(3);
+                            String[] split = tableName.split("\\.");
+                            map.put("database",split[0]);
+                            map.put("table",split[1]);
+                            map.put("action","update");
+                            map.put("where",shuzuToList(row.getKey()));
+                            map.put("chage",shuzuToList(row.getValue()));
+                            System.out.println("update数据："+map);
+                            KafkaProducerDemo.send(topic, JSON.toJSONString(map));
+                        }
+                    }
+                }
+                // insert数据
+                else if (data instanceof WriteRowsEventData) {
+                    WriteRowsEventData writeRowsEventData = (WriteRowsEventData) data;
+                    String tableName = tableMap.get(writeRowsEventData.getTableId());
+                    if (tableName != null && databaseList.contains(tableName)) {
+                        for (Serializable[] row : writeRowsEventData.getRows()) {
+                            Map<String,Object> map = new HashMap<>(3);
+                            String[] split = tableName.split("\\.");
+                            map.put("database",split[0]);
+                            map.put("table",split[1]);
+                            map.put("action","insert");
+                            map.put("where","");
+                            map.put("chage",shuzuToList(row));
+                            System.out.println("insert数据："+map);
+                            KafkaProducerDemo.send(topic, JSON.toJSONString(map));
+
+                        }
+                    }
+                }
+                // delete数据
+                else if (data instanceof DeleteRowsEventData) {
+                    DeleteRowsEventData deleteRowsEventData = (DeleteRowsEventData) data;
+                    String tableName = tableMap.get(deleteRowsEventData.getTableId());
+                    if (tableName != null && databaseList.contains(tableName)) {
+                        String eventKey = tableName + ".delete";
+                        for (Serializable[] row : deleteRowsEventData.getRows()) {
+                            Map<String,Object> map = new HashMap<>(3);
+                            String[] split = tableName.split("\\.");
+                            map.put("database",split[0]);
+                            map.put("table",split[1]);
+                            map.put("action","delete");
+                            map.put("where",shuzuToList(row));
+                            map.put("chage","");
+                            System.out.println("delete数据："+map);
+                            KafkaProducerDemo.send(topic, JSON.toJSONString(map));
+
+                        }
+                    }
+                }
+            }
+        }));
+        client.connect();
+    }
+
+    private List<Object> shuzuToList(Object[] objects){
+        List<Object> objectList = new ArrayList<>(objects.length);
+        for (int i = 0; i <objects.length ; i++) {
+            objectList.add(objects[i]);
+        }
+        return objectList;
+    }
+}
